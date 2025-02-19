a2ccb3de1ffd4a50b661494ffcca481ad9e9a4ac
konstantin <konstantinov.k@dex-it.ru>
2025-02-19 23:26:33 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import Greeting from "./Greeting.jsx";
+
+const App = () => {
+    return (
+        <Greeting name="Konstantin"/>
+    )
+}
+
+export default App
---
+++ src/Greeting.jsx
@@ -0,0 +1,7 @@
+const Greeting = ({name}) => {
+    return (
+        <h1>Привет, {name}!</h1>
+    );
+}
+
+export default Greeting;
